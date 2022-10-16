7db06050d98a88f749189c4aff36d62f1e90de69
Ngenzi <mugunga5@gmail.com>
2022-10-17 01:38:24 +0200

    Perfom account ops

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,7 +4,6 @@ module Types
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
 
-    # TODO: remove me
     field :users, [Types::UserType], null: false do
       description "Returns a list of users in the martian library"
     end
@@ -17,23 +16,40 @@ module Types
       argument :id, ID, required: true
     end
 
-    field :size, Integer, null: false do
+    field :users_size, Integer, null: false do
       description "Returns the number of users"
     end
 
+    field :transation, Types::TransactionType, null: false do
+      description "Returns a transaction by ID"
+      argument :id, ID, required: true
+    end
+
+    field :latest_amount, Integer, null: false do
+      description "Returns the latest amount"
+    end
+
+    def transaction(id)
+      Transaction.find(id)
+    end
+
+    def transactions_size
+      Transaction.count
+    end
+
     def user(id:)
-      User.find(id)
+      User.includes(:transactions).find(id)
     end
 
     def users
-      User.all.preload(:transactions)
+      User.all.includes(:transactions)
     end
 
     def transactions
-      Transaction.all
+      Transaction.all.includes(:user)
     end
 
-    def size
+    def users_size
       User.count
     end
   end
