68dfca09a562596cc4fe817fb0375eb888e10ffc
Ngenzi <mugunga5@gmail.com>
2022-10-16 21:48:06 +0200

    Create user update

--- app/graphql/mutations/create_user_mutation.rb
+++ app/graphql/mutations/create_user_mutation.rb
@@ -1,51 +1,33 @@
 module Mutations
   class CreateUserMutation < BaseMutation
+
+    class AuthData < Types::BaseInputObject
+      argument :credentials, Types::AuthInputTypes, required: false
+    end
+
     field :user, Types::UserType, null: false do
       description "The user that was created"
     end
     field :errors, [String], null: false do
       description "Errors that prevented the user from being created"
     end
+
     argument :name, String, required: true do
       description "Name of the user"
     end
 
-    argument :email, String, required: true do
-      description "Email of the user"
+    argument :auth_provider, AuthData, required: false do
+      description "Provider credentials for the user"
     end
 
-    argument :password, String, required: true do
-      description "Password of the user"
-    end
+    type Types::UserType
 
-    def resolve(name:, email:, password:)
-      user = User.new(name: name, email: email, password: password)
-      if user.save
-        {
-          user: user,
-          errors: []
-        }
-      else
-        {
-          user: nil,
-          errors: user.errors.full_messages
-        }
-      end
+    def resolve(name: nil, auth_provider: nil)
+      User.create!(
+        name: name,
+        email: auth_provider&.[](:credentials)&.[](:email),
+        password: auth_provider&.[](:credentials)&.[](:password)
+      )
     end
-
-    # def resolve(name:)
-    #   user = User.new(name: name)
-    #   if user.save
-    #     {
-    #       user: user,
-    #       errors: []
-    #     }
-    #   else
-    #     {
-    #       user: nil,
-    #       errors: user.errors.full_messages
-    #     }
-    #   end
-    # end
   end
 end
